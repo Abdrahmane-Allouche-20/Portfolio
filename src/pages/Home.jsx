3e00032e94d6c6ca1d8ccaf74a4c36231e1f1698
Ａｂｄｒａｈｍａｎｅ <121450231+Abdrahmaneallouche@users.noreply.github.com>
2025-04-17 22:00:31 +0200

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense, useEffect, useRef, useState } from "react";
 import HomeInfo from "../components/HomeInfo";
 import Loader from "../components/Loader";
-import Island  from "../models/island";
+import Island  from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import  Plane  from "../models/Plane";
